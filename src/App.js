6519efe477df60a9e14572954739a56daee35ded
rushabh9430 <sutariyarushabh2@gmail.com>
2023-01-12 09:30:46 +0530

    add data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes , Route } from 'react-router-dom';
+// import './App.css';
+import Department from './pages/Department';
+import Home from './pages/Home.js'
+import Doctors from './pages/doctors'
+import Contact from './pages/contact'
+import About from './pages/About'
+import Appointment from './pages/appointment'
+import Layout from './components/Layout.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+            <Route path='/' element={<Layout><Home /></Layout>}/>
+            <Route path='/Departments' element={<Layout><Department /></Layout>}/>
+            <Route path='/doctors' element={<Layout><Doctors /></Layout>}/>
+            <Route path='/contact' element={ <Layout><Contact /></Layout>}/>
+            <Route path='/about' element={<Layout><About /></Layout>}/>
+            <Route path='/appointment' element={<Layout><Appointment /></Layout>}/>
+        </Routes>
     </div>
   );
 }
---
+++ src/pages/doctors.js
@@ -0,0 +1,94 @@
+import React from "react";
+
+const doctors = () => {
+
+    const doctor = [
+        {
+            img : "../assets/img/doctors/doctors-1.jpg" ,
+            name : "Atha Smith",
+            position : "Chief Medical Officer",
+            dec: "Duis sagittis rutrum neque, quis tincidunt arcu pretium ac."
+
+        },
+        {
+            img : "../assets/img/doctors/doctors-2.jpg" ,
+            name : "John White",
+            position : "Anesthesiologist",
+            dec: "Aenean ac turpis ante. Mauris velit sapien."
+
+        },
+        {
+            img : "../assets/img/doctors/doctors-3.jpg" ,
+            name : "Umika Loha",
+            position : "Cardiology",
+            dec: "Curabitur luctus eleifend odio. Phasellus placerat mi."
+
+        },
+        {
+            img : "../assets/img/doctors/doctors-4.jpg" ,
+            name : "Daimy Smith",
+            position : "Neurosurgeon",
+            dec: "Morbi vulputate, tortor nec pellentesque molestie, eros nisiornare purus."
+
+        }
+    ]
+  return (
+    <div>
+      <section id="doctors" className="doctors">
+        <div className="container">
+          <div className="section-title">
+            <h2>Doctors</h2>
+            <p>
+              Duis sagittis rutrum neque, quis tincidunt arcu pretium ac.
+              Suspendisse sem risus, molestie vitae arcu et, tincidunt viverra
+              erat. Quisque in lectus id nulla viverra sodales in a risus.
+              Aliquam ut sem ex. Duis viverra ipsum lacus, ut pharetra arcu
+              sagittis nec. Phasellus a eleifend elit.
+            </p>
+          </div>
+          <div className="row">
+            {
+                doctor.map( (i) => {
+                    return(
+                        <div className="col-lg-6">
+              <div className="member d-flex align-items-start">
+                <div className="pic">
+                  <img
+                    src={i.img}
+                    className="img-doctor"
+                    alt
+                  />
+                </div>
+                <div className="member-info">
+                  <h4>{i.name}</h4>
+                  <span>{i.position}</span>
+                  <p>{i.dec}</p>
+                  <div className="social">
+                    <a href>
+                      <i className="ri-twitter-fill" />
+                    </a>
+                    <a href>
+                      <i className="ri-facebook-fill" />
+                    </a>
+                    <a href>
+                      <i className="ri-instagram-fill" />
+                    </a>
+                    <a href>
+                      {" "}
+                      <i className="ri-linkedin-box-fill" />{" "}
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+                    )
+                })
+            }
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default doctors;
