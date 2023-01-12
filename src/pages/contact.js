1a45f1e75fba73f90f812c7ba8ae4e2afaaf1e3a
rushabh9430 <sutariyarushabh2@gmail.com>
2023-01-12 09:58:25 +0530

    pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ const contact = () => {
           <div className="section-title">
             <h2>Contact</h2>
             <p>
-              Aenean enim orci, suscipit vitae sodales ac, semper in ex. Nunc
+              Aenean enim orci, dhgughj vitae sodales ac, semper in ex. Nunc
               aliquam eget nibh eu euismod. Donec dapibus blandit quam volutpat
               sollicitudin. Aenean ac turpis ante. Mauris velit sapien, aliquet
               aliquet rhoncus quis, luctus at neque. Mauris sit amet massa sed
